3ccb5b2270e06d5bff9e50361947e6879d6ca244
Aqso90 <kapten.bintang90@gmail.com>
2020-05-04 23:47:47 +0700

    perubahan1

--- js/bintangvalidasi.js
+++ js/bintangvalidasi.js
@@ -9,9 +9,16 @@ $(document).ready(function() {
         //checking for blank fields
         if (name == '' || email == '' || subject == '' || message == '') {
             alert("Please fill all fields..!!!");
+        } else if (!validateEmail(email)) {
+            alert("email salah");
         } else {
             alert("Your e-mail message was sent successfully");
             $("#contactform").trigger('reset');
         }
     });
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
